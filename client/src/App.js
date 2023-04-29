76b721bac89e9df37950f5dbeeb5211d7dc236c0
Meep <mwraich28@gmail.com>
2023-04-29 16:31:21 -0400

    Connect backend and frontend API

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      My App
-    </div>
-  );
+	const [response, setResponse] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch('http://localhost:8000/api', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			const data = await response.json();
+			setResponse(data);
+		} catch (error) {
+			console.error('Error fetching result:', error);
+		}
+	};
+	return (
+		<div>
+			<button onClick={handleSubmit}>Submit</button>
+			{response !== null && <p>Response: {response}</p>}
+		</div>
+	);
 }
 
 export default App;
