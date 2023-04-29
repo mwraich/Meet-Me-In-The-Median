968cb02aaa3e6994d97ba6b36e68ff54677e5c60
Meep <mwraich28@gmail.com>
2023-04-29 16:40:22 -0400

    Connect client and backend for base success case

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,39 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-	const [response, setResponse] = useState('');
+	const [number, setNumber] = useState('');
+	const [result, setResult] = useState(null);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const response = await fetch('http://localhost:8000/api', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			});
-			const data = await response.json();
-			setResponse(data);
-		} catch (error) {
-			console.error('Error fetching result:', error);
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:8000/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({number})
+      })
+      const data = await response.json()
+      setResult(data.result)
+    } catch (error) {
+      console.error('Error fetching result:', error)
+    }
+  }
 	return (
 		<div>
-			<button onClick={handleSubmit}>Submit</button>
-			{response !== null && <p>Response: {response}</p>}
+			<form onSubmit={handleSubmit}>
+				<label>
+					Enter a number:
+					<input
+						type="number"
+						value={number}
+						onChange={(e) => setNumber(e.target.value)}
+					/>
+					<button type="submit">Submit</button>
+				</label>
+			</form>
+			{result !== null && <p>Result: {result.join(', ')}</p>}
 		</div>
 	);
 }
